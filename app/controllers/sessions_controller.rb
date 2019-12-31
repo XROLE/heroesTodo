cec1f519d9cdd393ea594f17b7b2ae5940ccfd71
xrole <xrolediamond@gmail.com>
2019-12-31 06:42:14 +0100

    create new page for adding and viewing todos refactor welcome page style change page to render on succesfle register and login refactor welcome page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     @user = User.find_by_email(params['user']['email'])
     if @user && @user.authenticate(params['user']['password'])
       session[:user_id] = @user.id
-      redirect_to root_url, notice: "Logged in!"
+      render '/todos/new', notice: "Logged in!"
     else
       flash.now[:alert] ="Email or Password is incorrect"
       render "welcome/index"
